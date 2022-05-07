e977e3848a22997c8dc3b6338281ec1b684fc2db
imaun <imun22@gmail.com>
2022-05-08 01:37:47 +0430

    initial commit

---
+++ src/Emun.GAuth/GoogleAuthenticator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Drawing;
+using System.Security.Cryptography;
+using Emun.GAuth.Extensions;
+
+namespace Emun.GAuth
+{
+
+    public class GoogleAuthenticator
+    {
+
+        private static readonly DateTime _epoch =
+            new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+
+        private byte[] _byteArr = new byte[10];
+
+        private long getCounter(int counter)
+            => Convert.ToInt64((DateTime.UtcNow - _epoch).TotalSeconds / counter);
+
+        public string GetStupCode(string account, string appName, byte[] secrectKey) {
+            if(string.IsNullOrEmpty(account)) {
+                throw new ArgumentNullException("Account name is null or empty.");
+            }
+
+            var normalizedAccount = account.RemoveWhiteSpaces();
+
+            var encodedSecretKey = Base32Encoding.ToBase32String(secrectKey);
+
+            //TODO 
+            return null;
+
+        }
+    }
+}
---
+++ src/Emun.GAuth/Internal/Exts.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Emun.GAuth.Extensions
+{
+    public static class Exts
+    {
+
+        public static string RemoveWhiteSpaces(this string str) {
+            return new string(str.Where(c => !char.IsWhiteSpace(c)).ToArray());
+        }
+
+    }
+}
