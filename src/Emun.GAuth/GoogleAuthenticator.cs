983772f0fee8db54f705d12e33b12082e5f79c13
imun <imun22@gmail.com>
2022-05-09 17:13:58 +0430

    Add methods to calculate HASH from secretKey

--- src/Emun.GAuth/GoogleAuthenticator.cs
+++ src/Emun.GAuth/GoogleAuthenticator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Drawing;
+using System.Collections.Generic;
 using System.Security.Cryptography;
 using Emun.GAuth.Extensions;
 
@@ -12,24 +13,66 @@ namespace Emun.GAuth
         private static readonly DateTime _epoch =
             new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
 
-
+        private int _defaultSeconds = 30;
         private byte[] _byteArr = new byte[10];
 
-        private long getCounter(int counter)
+        private long getCounter(long counter)
             => Convert.ToInt64((DateTime.UtcNow - _epoch).TotalSeconds / counter);
 
-        public string GetStupCode(string account, string appName, byte[] secrectKey) {
+        private long getDefaultCounter() => getCounter(_defaultSeconds);
+
+        public string GetStupCode(string account, string appName, string secrectKey) {
             if(string.IsNullOrEmpty(account)) {
                 throw new ArgumentNullException("Account name is null or empty.");
             }
 
-            var normalizedAccount = account.RemoveWhiteSpaces();
+            var encodedSecretKey = secrectKey.ConvertToBytes(isBase32: true);
+            var normalizedAccount = Uri.EscapeUriString(account.RemoveWhiteSpaces());
+            //TODO generate qrcode
 
-            var encodedSecretKey = Base32Encoding.ToBase32String(secrectKey);
-
-            //TODO 
-            return null;
-
+            var result = Base32Encoding.ToBase32String(encodedSecretKey);
+            
+            return result.Trim('=');
         }
+        
+
+        public string GeneratePIN(string secretKey, long counter, int codeLength = 6) {
+            var secretKeyBytes = secretKey.ConvertToBytes(isBase32: true);
+            return secretKeyBytes.ComputeHashCode(getCounter(counter), codeLength);
+        }
+
+        public string GetCurrentPIN(string secrectKey, int codeLength = 6) {
+            var secretKeyBytes = secrectKey.ConvertToBytes(isBase32: true);
+            return secretKeyBytes.ComputeHashCode(getDefaultCounter(), codeLength);
+        }
+
+
+        /// <summary>
+        /// Get All the possible PIN codes for the given timeWindow.
+        /// </summary>
+        /// <param name="secretKey"></param>
+        /// <param name="timeWindow"></param>
+        /// <param name="codeLength"></param>
+        /// <returns></returns>
+        public List<string> GetAllPINS(string secretKey, TimeSpan timeWindow, int codeLength = 6) {
+            var result = new List<string>();
+            var counter = getDefaultCounter();
+            var offset = 0;
+
+            if(timeWindow.TotalSeconds > _defaultSeconds) {
+                offset = Convert.ToInt32(timeWindow.TotalSeconds / _defaultSeconds);
+            }
+
+            var start = counter - offset;
+            var end = counter + offset;
+
+            //generates all PINs within the given timeWindow
+            for(var c = start; c <= end; c++) {
+                result.Add(GeneratePIN(secretKey, c, codeLength));
+            }
+
+            return result;
+        }
+        
     }
 }
--- src/Emun.GAuth/Internal/Exts.cs
+++ src/Emun.GAuth/Internal/Exts.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Collections.Generic;
 using System.Text;
 
@@ -11,6 +12,35 @@ namespace Emun.GAuth.Extensions
         public static string RemoveWhiteSpaces(this string str) {
             return new string(str.Where(c => !char.IsWhiteSpace(c)).ToArray());
         }
+
+
+        public static byte[] ConvertToBytes(this string key, bool isBase32) {
+            return isBase32 
+                ? Base32Encoding.ToBytes(key)
+                : Encoding.UTF8.GetBytes(key);
+        }
+        
+        public static string ComputeHashCode(this byte[] key, long step, int codeLength) {
+            var counter = BitConverter.GetBytes(step);
+
+            if(BitConverter.IsLittleEndian) {
+                Array.Reverse(counter);
+            }
+
+            var hMACSH1 = new HMACSHA1(key);
+            var hash = hMACSH1.ComputeHash(counter);
+            var offset = hash[hash.Length - 1] & 0xf;
+
+            var intnum = ((hash[offset] & 0x7f) << 24)
+                | (hash[offset + 1] << 16)
+                | (hash[offset + 2] << 8)
+                | hash[offset + 3];
+                
+            var result = intnum = % Convert.ToInt32(Math.Pow(10, codeLength));
+            return result.ToString(new string('0', codeLength));
+        }
+
+
 
     }
 }
